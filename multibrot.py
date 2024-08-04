27a7ea704d639a3396850e7be390470e6e4dcc02
perturbedpear <161806640+Kryt3n@users.noreply.github.com>
2024-08-04 15:19:01 +1000

    more pyplot support
    Added more functionality to pyplot.
    Cleaned up some comments and unused code.

--- multibrot.py
+++ multibrot.py
@@ -7,6 +7,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import random
+import time
+import os
+
+
 
 pi = cmath.pi
 e = cmath.e
@@ -29,13 +33,6 @@ def prime_sieve(end):
             composite_integers = np.union1d(composite_integers, mult_range)
     integers = np.delete(integers, composite_integers)
     return integers
-print(prime_sieve(30))
-
-# def is_prime(number):
-#     for i in range(2, sqrt(number))
-
-def get_factors(number, exponent = 0, include_trivial=False):
-    pass
 
 class Configuration(metaclass=ABCMeta):
     @classmethod
@@ -58,6 +55,10 @@ class MultibrotAttrs(Configuration):
         z_next = (instance.z_i)**(instance.exponent)+instance.z_0
         return z_next
     @staticmethod
+    def julia_set(instance):
+        z_next = (instance.z_i)**(instance.exponent)+exp(complex(0,pi/2))
+        return z_next
+    @staticmethod
     def multibrot_iter_map(instance):
         z_next = (instance.z_i)**(instance.get_iteration())+instance.z_0
         return z_next
@@ -73,36 +74,19 @@ class MultibrotAttrs(Configuration):
     def map_z_0(instance):
         z_next = (instance.z_i)**(instance.z_0)+instance.z_0
         return z_next
-    def other(instance):
-        z_next = (instance.z_i)**(2)+2*instance.z_i+instance.z_0
+    @staticmethod
+    def exp_x_iy(instance):
+        z_next = exp(instance.z_i)+instance.z_0
         return z_next
-    #default_fn = mandelbrot_map
-    #default_fn = multibrot_iter_map
-    #default_fn = sq_iter_map
+    @staticmethod
+    def inverse_julia(instance):
+        z_next = ((instance.z_i)**(2)+exp(complex(0, pi/2)))**(complex(0, -1))
+        return z_next
+    @staticmethod
+    def gamma(instance):
+        z_next = None
+        return z_next
     default_fn = mandelbrot_map
-
-    # def rec_relation_n_plus_1(self):
-    #     if abs(self.hc) != 1:
-
-    #         print(self.hc)
-    #         new_re = (self.re**2+self.c_re)/(1-self.hc**2)
-    #         new_hc = (2*self.re*self.hc+self.c_hc)/(1-self.hc**2) 
-    #         self.re = new_re
-    #         self.hc = new_hc
-    #         self.cache.append((new_re, new_hc))
-    #     else:
-    #         self.passes = self.depth
-
-    # def rec_relation_n_plus_k(self, depth):
-    #     for i in range(1, depth):
-    #         self.rec_relation_n_plus_1()
-    #     return self.get_cache()
-"""
-#TEST FOR Multibrot
-MultibrotAttrs.with_kwargs(escape = 4, max_iteration = 10000, boobies=80085) #exclude this line for default vals.
-test = Multibrot(-1, 0)
-print(test.do_map_iteration())
-"""
 
 class Multibrot():  
     attrs = MultibrotAttrs
@@ -115,7 +99,7 @@ class Multibrot():
         self.re_i, self.im_i = self.re_0, self.im_0
         self.z_0             = complex(re_0 , im_0)
         self.z_i             = self.z_0
-        self.exponent        = complex(exponent_re,exponent_im)  #(random.uniform(0, 2), random.uniform(0, 2)) #delete when done screwing around# complex(exponent_re, exponent_im)
+        self.exponent        = complex(exponent_re,exponent_im)
         self.z_fn_string     = z_fn_string
 
         self._attrs     = self.get_attributes()
@@ -123,7 +107,6 @@ class Multibrot():
         self._cache     = [self.z_0]
         self._last_z         = None
         self._last_iteration = None
-
         self._set_function()
 
     def get_iteration(self):
@@ -174,7 +157,6 @@ class Multibrot():
             return self.get_last_iteration()
             
     def get_last_iteration(self):
-        # vvv for when this ^^^ function is invoked without do_map_iteration first.
         if self._iteration == 0:
             print("No operation has yet been performed on z_0 to produce a result.")
             return -1
@@ -194,30 +176,59 @@ class FractalplotAttrs(Configuration):
     pass
 
 class Fractalplot():
-    def __init__(self, re_start, re_stop, im_start, im_stop, resolution):
+    def __init__(self, re_start, re_stop, im_start, im_stop, resolution_x, resolution_y):
         self.re_start   = re_start
         self.re_stop    = re_stop
         self.im_start   = im_start
         self.im_stop    = im_stop
-        self.resolution = resolution
+        self.resolution_x = resolution_x
+        self.resolution_y = resolution_y
+        
 
-        self.lin1 = np.linspace(self.re_start, self.re_stop, resolution)
-        self.lin2 = np.linspace(self.im_start, self.im_stop, resolution)
+        self.lin1 = np.linspace(self.re_start, self.re_stop, self.resolution_x)
+        self.lin2 = np.linspace(self.im_start, self.im_stop, self.resolution_y)
         self.RE, self.IM = np.meshgrid(self.lin1, self.lin2)
         self.evaluate_all_maps = np.vectorize(self.evaluate_map)
         self.iteration_maps = self.evaluate_all_maps(self.RE, self.IM)
 
-        fig, axs = plt.subplots(layout='constrained')
-        pc = axs.pcolormesh(self.RE, self.IM, self.iteration_maps, vmin=0, vmax=maxitr, cmap='twilight_shifted')
-        axs.set_title('Mandelbrot Set')
-        plt.show()
+        self.make_plot_dir()
+
+        self.fig, self.axs = plt.subplots(layout='constrained')
+        self.pc = self.axs.pcolormesh(self.RE, self.IM, self.iteration_maps, vmin=0, vmax=maxitr, cmap='twilight_shifted')
+
+        self.axs.set_aspect('equal', adjustable='box')
+        self.save_plot_to_folder(draw_features = 'off')
+        self.axs.set_title('Mandelbrot Set')
+        plt.axis('on')
+        #plt.show()
 
     def evaluate_map(self, re, im):
-        return Multibrot(re, im, ln(pi), pi/2).do_map_iteration()
+        return Multibrot(re, im).do_map_iteration()
+    
+    def make_plot_dir(self):
+        #🤮
+        split = os.path.split
+        abspath = os.path.abspath
+        self.image_path = split(split(abspath(__file__))[0])[0] 
+        if not os.path.exists(self.image_path+'/plots'):
+            os.mkdir(self.image_path+'/plots')
+        self.image_path = self.image_path + '/plots/'
 
-maxitr =25
+    
+    def save_plot_to_folder(self, draw_features):
+        plt.axis(draw_features)
+        self.image_name = f'Fractal-{time.time()}.svg'
+        self.fig.savefig(self.image_path+self.image_name, format='svg', bbox_inches = 'tight', pad_inches = 0) #dpi = (mbp_dpi*dpi_scale))
+
+resolution_x = 2560*3
+resolution_y = 1600*3
+#mbp_dpi = 300
+#dpi_scale = 3
+maxitr = 25
 MultibrotAttrs.with_kwargs(escape = 10, max_iteration = maxitr, enable_return = True)
-#fractest = Fractalplot(-1,2.7, -1, 1, 1000)
-
-
+fractest = Fractalplot(-2.5, 0.5, -1.25, 1.25, resolution_x, resolution_y) #resolution_x*dpi_scale, resolution_y*dpi_scale)#resolution)
+# mbp 2019 2560x1600 @ 227 dpi
 #mandelbrot bounds: -2.5,0.5, -1.25, 1.25, 200
+
+
+
